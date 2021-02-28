4cab34d34360d50527decaf89f538641ee7602d2
Austin Ruhf <austin.ruhf@gmail.com>
2021-02-28 14:05:27 -0500

    broke testpage into handlebars

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,16 @@
-Create the burgers_db.
-Switch to or use the burgers_db.
-Create a burgers table with these fields:
-id: an auto incrementing int that serves as the primary key.
-burger_name: a string.
-devoured: a boolean.
+-- Create the burgers_db.
+-- Switch to or use the burgers_db.
+-- Create a burgers table with these fields:
+-- id: an auto incrementing int that serves as the primary key.
+-- burger_name: a string.
+-- devoured: a boolean.
+CREATE DATABASE burger_db;
+
+USE burger_db;
+
+CREATE TABLE burgers (
+    id int NOT NULL AUTO_INCREMENT,
+    burger_name varchar(255) NOT NULL,
+    devoured boolean NOT NULL DEFAULT false,
+    PRIMARY KEY (id)
+);
