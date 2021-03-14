7020bee4226225fcda4361ae7cc592a06a947957
Austin Ruhf <austin.ruhf@gmail.com>
2021-03-13 20:03:41 -0500

    hid MySQL connections

--- config/connection.js
+++ config/connection.js
@@ -5,11 +5,11 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        host: "pfw0ltdr46khxib3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        host: process.env.DB_HOST,
         port: 3306,
-        user: "b617574ntfky4i1s",
-        password: "nbzv4hcrxn63v8gg",
-        database: "dey04kozh2hxva37"
+        user: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE
     });
 }
 
